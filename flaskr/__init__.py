33859274e9e8c6ab9108fd8a1abcf16f250c4d95
Odol Shinu <odolshinu@gmail.com>
2020-04-09 16:03:37 +0530

    blog blueprint added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,7 +31,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from . import auth
+    from . import auth, blog
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
